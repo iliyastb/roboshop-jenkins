e1e66842d1479093f6965be66ee2cf7e9f016e2b
iliyastb <skiliyas300@gmail.com>
2024-05-07 15:48:59 +0530

    Update

--- vars.tf
+++ vars.tf
@@ -1,6 +1,6 @@
 variable "jobs" {
   default = [
     { name = "roboshop", folder = "Infra",repo_url = "https://github.com/iliyastb/roboshop-infra.git" },
-    { name = "roboshop-dev", folder = "Infra-dev",repo_url = "https://github.com/iliyastb/roboshop-infra.git" }
+    { name = "roboshop-dev", folder = "Infra",repo_url = "https://github.com/iliyastb/roboshop-infra.git" }
   ]
 }
