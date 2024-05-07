43e88b65cd02e0585bab4c9ac9f84424552db5d3
iliyastb <skiliyas300@gmail.com>
2024-05-07 19:17:11 +0530

    Update

--- vars.tf
+++ vars.tf
@@ -1,9 +1,15 @@
 variable "folders" {
-  default = ["Infra"]
+  default = ["Infra", "CI-Pipeline"]
 }
 
 variable "jobs" {
   default = [
-    { name = "roboshop", folder = "Infra", repo_url = "https://github.com/iliyastb/roboshop-infra.git" }
+    { name = "roboshop", folder = "Infra", repo_url = "https://github.com/iliyastb/roboshop-infra.git" },
+    { name = "frontend", folder = "CI-Pipeline", repo_url = "https://github.com/iliyastb/roboshop-infra.git" },
+    { name = "catalogue", folder = "CI-Pipeline", repo_url = "https://github.com/iliyastb/roboshop-infra.git" },
+    { name = "user", folder = "CI-Pipeline", repo_url = "https://github.com/iliyastb/roboshop-infra.git" },
+    { name = "cart", folder = "CI-Pipeline", repo_url = "https://github.com/iliyastb/roboshop-infra.git" },
+    { name = "shipping", folder = "CI-Pipeline", repo_url = "https://github.com/iliyastb/roboshop-infra.git" },
+    { name = "payment", folder = "CI-Pipeline", repo_url = "https://github.com/iliyastb/roboshop-infra.git" }
   ]
 }
