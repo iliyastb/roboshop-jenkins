cafa0a84e46b247463b8ecc2778120bc4b2fd21d
iliyastb <skiliyas300@gmail.com>
2024-05-07 21:50:33 +0530

    Update

--- vars.tf
+++ vars.tf
@@ -5,11 +5,11 @@ variable "folders" {
 variable "jobs" {
   default = [
     { name = "roboshop", folder = "Infra", repo_url = "https://github.com/iliyastb/roboshop-infra.git" },
-    { name = "frontend", folder = "CI-Pipeline", repo_url = "https://github.com/iliyastb/roboshop-infra.git" },
-    { name = "catalogue", folder = "CI-Pipeline", repo_url = "https://github.com/iliyastb/roboshop-infra.git" },
-    { name = "user", folder = "CI-Pipeline", repo_url = "https://github.com/iliyastb/roboshop-infra.git" },
-    { name = "cart", folder = "CI-Pipeline", repo_url = "https://github.com/iliyastb/roboshop-infra.git" },
-    { name = "shipping", folder = "CI-Pipeline", repo_url = "https://github.com/iliyastb/roboshop-infra.git" },
-    { name = "payment", folder = "CI-Pipeline", repo_url = "https://github.com/iliyastb/roboshop-infra.git" }
+    { name = "frontend", folder = "CI-Pipeline", repo_url = "https://github.com/iliyastb/frontend.git" },
+    { name = "catalogue", folder = "CI-Pipeline", repo_url = "https://github.com/iliyastb/catalogue.git" },
+    { name = "user", folder = "CI-Pipeline", repo_url = "https://github.com/iliyastb/user.git" },
+    { name = "cart", folder = "CI-Pipeline", repo_url = "https://github.com/iliyastb/cart.git" },
+    { name = "shipping", folder = "CI-Pipeline", repo_url = "https://github.com/iliyastb/shipping.git" },
+    { name = "payment", folder = "CI-Pipeline", repo_url = "https://github.com/iliyastb/payment.git" }
   ]
 }
