1a44c9820c18024f03d554e6c05b70042951d183
iliyastb <skiliyas300@gmail.com>
2024-05-07 15:17:16 +0530

    Update

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    jenkins = {
+      source = "registry.terraform.io/taiidani/jenkins"
+    }
+  }
+}
+
 provider "jenkins" {
   server_url = "http://3.80.75.201:8080/"
   username   = data.aws_ssm_parameter.jenkins_user.value
